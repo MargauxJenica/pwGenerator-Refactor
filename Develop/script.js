8b4f358c356222a8069be4a94796d96274c55658
Margaux <santos@Margauxs-Mini.lan>
2023-12-18 22:02:11 -0800

    js variables added

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,16 @@
+// Clicked "Generate Password" button prompts user password criteria
+// Password must be 8-128 characters
+// User decides on password criterias, there has to be at least 1 type 
+// Password is generated based on users selected criteria 
+// Displayed or Alerted
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-=+[]{}|:;<.>/?";
 
 // Write password to the #password input
 function writePassword() {
